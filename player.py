45f7360687c9868efabc56d590ccf068b82702ff
lugialo <gabrielzin911@gmail.com>
2023-06-28 13:54:18 -0300

    Waves don't die

--- player.py
+++ player.py
@@ -4,7 +4,6 @@ with contextlib.redirect_stdout(None):
     from pygame import mixer
 import os
 from os import system
-import threading
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
@@ -23,18 +22,7 @@ mixer.music.set_volume(0.5)
 mixer.music.play()
 
 
-def play_music():
-    pygame.mixer.music.play()
-    pygame.mixer.music.set_endevent(pygame.USEREVENT+1)
-
-
-music_thread = threading.Thread(target=play_music)
-music_thread.start
-
-
 while True:
-
-    next_music_index = (current_music_index + 1) % len(music_files)
 
     if mixer.music.get_busy():
         print("Press 'P' to pause!")
@@ -43,19 +31,6 @@ while True:
     print("Press 'C' to change music!")
     print("Press 'V' to set volume!")
     print("Press 'E' to exit!")
-
-    for event in pygame.event.get():
-        if event.type == pygame.USEREVENT+1:
-            current_music_index = next_music_index
-            mixer.music.load(file_path[current_music_index])
-            mixer.music.play()
-
-    if not mixer.music.get_busy():
-        current_music_index = next_music_index
-        file_name = music_files[current_music_index]
-        file_path = os.path.join(musics_dir, file_name)
-        mixer.music.load(file_path)
-        mixer.music.play()
 
     ch = input("['p', 'r', 'v', 'c', 'e']>>> ")
     system("cls")
@@ -73,7 +48,6 @@ while True:
         file_path = os.path.join(musics_dir, input_file_name)
         mixer.music.load(f"{file_path}.mp3")
         mixer.music.play()
-        current_music_index = music_files.index(input_file_name + ".mp3")
         system("cls")
     elif ch == "e":
         exit()
--- tests.py
+++ tests.py
@@ -1,60 +1,67 @@
-import pygame
-from pygame import mixer
+import contextlib
+with contextlib.redirect_stdout(None):
+    import pygame
+    from pygame import mixer
 import os
+from os import system
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 musics_dir = os.path.join(os.getcwd(), "musics")
+
 music_files = os.listdir(musics_dir)
 current_music_index = 0
-mixer.init()
+
+pygame.mixer.init()
 pygame.display.init()
-mixer.music.set_volume(0.5)
-
-# create a clock object to control the timing
-clock = pygame.time.Clock()
 
 
-def play_music():
+def play_next_music():
     global current_music_index
-    while True:
-        next_music_index = (current_music_index + 1) % len(music_files)
-        file_name = music_files[next_music_index]
-        file_path = os.path.join(musics_dir, file_name)
-        mixer.music.load(file_path)
-        mixer.music.play()
-        current_music_index = next_music_index
-        # wait for the music to finish playing before loading the next one
-        while mixer.music.get_busy():
-            clock.tick(30)  # set the frame rate to 30 fps
+    current_music_index = (current_music_index + 1) % len(music_files)
+    file_name = music_files[current_music_index]
+    file_path = os.path.join(musics_dir, file_name)
+    mixer.music.load(file_path)
+    mixer.music.play()
 
+
+file_name = "001nao-ha-ferrolhos.mp3"
+file_path = os.path.join(musics_dir, file_name)
+mixer.music.load(file_path)
+mixer.music.set_volume(0.5)
+mixer.music.play()
 
 while True:
-    print("Press 'P' to pause!")
-    print("Press 'R' to resume!")
+    if mixer.music.get_busy():
+        print("Press 'P' to pause!")
+    else:
+        print("Press 'R' to resume!")
     print("Press 'C' to change music!")
     print("Press 'V' to set volume!")
     print("Press 'E' to exit!")
-    event = pygame.event.wait()
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_p:
-            mixer.music.pause()
-        elif event.key == pygame.K_r:
-            mixer.music.unpause()
-        elif event.key == pygame.K_v:
-            try:
-                v = float(input("Enter volume (0 to 1): "))
-                if 0 <= v <= 1:
-                    mixer.music.set_volume(v)
-                else:
-                    print("Invalid volume value!")
-            except ValueError:
-                print("Invalid input!")
-        elif event.key == pygame.K_c:
-            input_file_name = input(
-                "Enter the name of the song (without .mp3 extension). \nNOTE: the song *MUST* be in the 'musics' folder.\n")
-            file_path = os.path.join(musics_dir, f"{input_file_name}.mp3")
-            if os.path.exists(file_path):
-                mixer.music.load(file_path)
-            else:
-                print("Invalid song name!")
-        elif event.key == pygame.K_e:
-            break
+
+    ch = input("['p', 'r', 'v', 'c', 'e']>>> ")
+    system("cls")
+    if ch == "p":
+        mixer.music.pause()
+    elif ch == "r":
+        mixer.music.unpause()
+    elif ch == "v":
+        v = float(input("Enter volume (0 to 1): "))
+        mixer.music.set_volume(v)
+        system("cls")
+    elif ch == "c":
+        input_file_name = input(
+            "Enter the name of the song (without .mp3 extension).\n"
+            "NOTE: the song *MUST* be in the 'musics' folder.\n"
+        )
+        file_path = os.path.join(musics_dir, input_file_name + ".mp3")
+        mixer.music.load(file_path)
+        mixer.music.play()
+        system("cls")
+    elif ch == "e":
+        exit()
+
+    # Check if music has ended
+    if not mixer.music.get_busy():
+        play_next_music()
